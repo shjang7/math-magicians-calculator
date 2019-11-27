a9e56a4121843eb8eb3af87df0d66031e9be4f74
shjang7 <lucy.sh.jang@gmail.com>
2019-11-27 23:11:37 +0900

    Add button click event handler

--- src/components/button_panel.js
+++ src/components/button_panel.js
@@ -4,38 +4,48 @@ import { color } from '../assets/style.json';
 
 const { lightGray: gray } = color;
 
-const ButtonPanel = () => (
-  <div className="button-panel d-flex flex-column">
-    <div className="flex-row group">
-      <Button buttonName="AC" color={gray} />
-      <Button buttonName="+/-" color={gray} />
-      <Button buttonName="%" color={gray} />
-      <Button buttonName="÷" />
+const ButtonPanel = ({ clickHandler }) => {
+  const handleClick = buttonName => clickHandler(buttonName);
+
+  const renderButton = ({ val, color }) => {
+    if (val === '0') return <Button buttonName={val} color={gray} wide clickHandler={handleClick} />;
+    if (color) return <Button buttonName={val} color={gray} clickHandler={handleClick} />;
+    return <Button buttonName={val} clickHandler={handleClick} />;
+  };
+
+  return (
+    <div className="button-panel d-flex flex-column">
+      <div className="flex-row group">
+        {renderButton({ val: 'AC', color })}
+        {renderButton({ val: '+/-', color })}
+        {renderButton({ val: '%', color })}
+        {renderButton({ val: '÷' })}
+      </div>
+      <div className="flex-row group">
+        {renderButton({ val: '7', color })}
+        {renderButton({ val: '8', color })}
+        {renderButton({ val: '9', color })}
+        {renderButton({ val: 'x' })}
+      </div>
+      <div className="flex-row group">
+        {renderButton({ val: '4', color })}
+        {renderButton({ val: '5', color })}
+        {renderButton({ val: '6', color })}
+        {renderButton({ val: '-' })}
+      </div>
+      <div className="flex-row group">
+        {renderButton({ val: '1', color })}
+        {renderButton({ val: '2', color })}
+        {renderButton({ val: '3', color })}
+        {renderButton({ val: '+' })}
+      </div>
+      <div className="flex-row group">
+        {renderButton({ val: '0', color })}
+        {renderButton({ val: '.', color })}
+        {renderButton({ val: '=' })}
+      </div>
     </div>
-    <div className="flex-row group">
-      <Button buttonName="7" color={gray} />
-      <Button buttonName="8" color={gray} />
-      <Button buttonName="9" color={gray} />
-      <Button buttonName="x" />
-    </div>
-    <div className="flex-row group">
-      <Button buttonName="4" color={gray} />
-      <Button buttonName="5" color={gray} />
-      <Button buttonName="6" color={gray} />
-      <Button buttonName="-" />
-    </div>
-    <div className="flex-row group">
-      <Button buttonName="1" color={gray} />
-      <Button buttonName="2" color={gray} />
-      <Button buttonName="3" color={gray} />
-      <Button buttonName="+" />
-    </div>
-    <div className="flex-row group">
-      <Button buttonName="0" color={gray} wide />
-      <Button buttonName="." color={gray} />
-      <Button buttonName="=" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default ButtonPanel;
