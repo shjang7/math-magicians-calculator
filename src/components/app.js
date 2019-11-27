7c06f0a7d7aef5ce33668987dbb230af0b66b935
shjang7 <lucy.sh.jang@gmail.com>
2019-11-27 16:54:41 +0900

    Add calling calculate function

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Display from './display';
 import ButtonPanel from './button_panel';
+import calculate from '../logic/calculate';
 
 const App = () => (
   <div id="main" className="main d-flex flex-column">
